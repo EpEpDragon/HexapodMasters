d71d3a592750613072b540aee4b50314f8c93205
epep <bergapie@gmail.com>
2023-03-02 15:15:07 +0200

    Dynamixel servo library added

---
+++ MyDynamixel/MyDynamixel.cpp
@@ -0,0 +1,203 @@
+#include "Arduino.h"
+#include "MyDynamixel.h"
+
+MyDynamixel::MyDynamixel(HardwareSerial& port, int rate, int Dir_pin): _port(port)
+{
+	dir_pin_ = Dir_pin;
+	_port.begin(rate);
+	_port.transmitterEnable(Dir_pin);
+	_port.setTimeout(100);
+}
+
+void MyDynamixel::WriteServos (uint8_t id, enum CONTROL_TABLE reg, uint8_t * data, uint8_t dataLength)
+{
+	uint8_t returnPack[6];
+	uint8_t lengt = dataLength + 3;
+	uint8_t packet[lengt + 4];
+
+	packet[0] = packet[1] = 0xFF;
+	packet[2] = id;
+	packet[3] = lengt;
+	packet[4] = WRITE_DATA;
+	packet[5] = reg;
+
+	for (int j = 0; j < dataLength; j++)
+	{
+		packet[6 + j] = data[j];
+	}
+
+
+	packet[lengt + 3] = 0;
+	for (int i = 2; i < lengt + 3; i++)
+	{
+		packet[lengt + 3] += packet[i];
+	}
+	packet[lengt + 3] = ~packet[lengt + 3];
+	
+	_port.write(packet, lengt + 4);
+	
+	_port.flush(); //wait for packet to be send
+	delayMicroseconds(300); //fixed return delay time
+	if(_port.available()>0)
+	{
+		uint8_t x=_port.readBytes(returnPack,6);
+		// for(int i = 0; i < 6+dataLength; i++)
+		// {
+			// Serial.print(returnPack[i],HEX);
+		// }
+		//Serial.println(x);
+	}
+	while(_port.read() >= 0){}
+}
+
+void MyDynamixel::ReadServos (uint8_t id, enum CONTROL_TABLE reg, uint8_t dataLength, uint8_t* returnPack)
+{
+	//uint8_t returnPack[6+dataLength];
+	uint8_t lengt = 4;
+	uint8_t readpacket[lengt + 4];
+
+	readpacket[0] = readpacket[1] = 0xFF;
+	readpacket[2] = id;
+	readpacket[3] = lengt;
+	readpacket[4] = READ_DATA;
+	readpacket[5] = reg;
+
+	readpacket[6] = dataLength;
+
+	readpacket[lengt + 3] = 0;
+	for (int i = 2; i < lengt + 3; i++) 
+	{
+		readpacket[lengt + 3] += readpacket[i];
+	}
+	readpacket[lengt + 3] = ~readpacket[lengt + 3];
+
+	_port.write(readpacket, lengt + 4);
+
+	_port.flush(); //wait for packet to be send
+	delayMicroseconds(300); //fixed return delay time	
+	if(_port.available()>0)
+	{
+		uint8_t x=_port.readBytes(returnPack,6+dataLength);
+		// for(int i = 0; i < 6+dataLength; i++)
+		// {
+		 // Serial.println(returnPack[i],HEX);
+		// }
+		// Serial.print(x);
+	}
+	while(_port.read() >= 0){}
+	//return returnPack;
+}
+
+void MyDynamixel::MoveServos (uint8_t id, double position, double speed)
+{
+	double pos = position/0.29;
+	if(pos < 0)
+	{
+		pos = 0;
+	}
+	else if(pos > 1023)
+	{
+		pos = 1023;
+	}
+	goalMovementBits[0] = (uint16_t)pos & 0xff;
+	goalMovementBits[1] = (uint16_t)pos >> 8;
+		
+	if(speed == -10)
+	{
+		WriteServos(id,GOAL_POSITION_L, goalMovementBits, 2);
+	}
+	else
+	{
+		double spd = speed/0.111;
+		goalMovementBits[2] = (uint16_t)(spd < 0 ? 1 : spd) & 0xff;
+		goalMovementBits[3] = (uint16_t)(spd < 0 ? 1 : spd) >> 8;
+		WriteServos(id,GOAL_POSITION_L, goalMovementBits, 4);
+	}
+}
+
+double MyDynamixel::PresentPos (uint8_t id)
+{
+	double position=0;
+	uint16_t pos=0;
+	uint8_t retPac[8]={0,0,0,0,0,0,0,0};
+	
+	ReadServos (id, PRESENT_POSITION_L, 2, retPac);
+	pos = (uint16_t)((retPac[6]& 0xFF) << 8) | (uint16_t)(retPac[5]& 0xFF);
+	position = (double)pos*0.29;
+	//Serial.println(position);
+	
+	return position;
+}
+
+void MyDynamixel::SyncWrite (uint8_t* id, uint8_t numMotors, enum CONTROL_TABLE reg, uint8_t * data, uint8_t dataLength)
+{
+	uint8_t length = (dataLength+1)*numMotors+4;
+	uint8_t BroadcastID = 0xFE;
+	uint8_t instruction = SYNC_WRITE;
+	
+	uint8_t packet[length+4];
+	
+	packet[0] = packet[1] = 0xFF;
+	packet[2] = BroadcastID;
+	packet[3] = length;
+	packet[4] = instruction;
+	packet[5] = reg;
+	packet[6] = dataLength;
+	
+	int j = 0;
+	int k = 0;
+	for(int i = 0; i<numMotors*(dataLength+1); i++)
+	{	
+		if (i%5 == 0)
+		{
+			packet[7+i] = id[j];
+			j = j + 1;
+		}
+		else
+		{
+			packet[7+i] = data[k];
+			k = k + 1;
+		}
+
+	}
+
+	packet[length + 3] = 0;
+	for (int i = 2; i < length + 3; i++)
+	{
+		packet[length + 3] += packet[i];
+	}
+	packet[length + 3] = ~packet[length + 3];
+
+	_port.write(packet, length + 4);
+	
+	_port.flush(); //wait for packet to be send
+
+}
+
+void MyDynamixel::SyncMove (uint8_t* id, double* position, double* speed, uint8_t numMotors)
+{
+	uint8_t dataLength = numMotors*4;
+	uint8_t data[dataLength];
+
+	for(int i = 0,j = 0; i<dataLength && j<numMotors; i+=4,j++)
+	{
+		double pos = position[j]/0.29;
+		if(pos < 0)
+		{
+			pos = 0;
+		}
+		else if(pos > 1023)
+		{
+			pos = 1023;
+		}
+		data[i] = (uint16_t)pos & 0xff;
+		data[i+1] = (uint16_t)pos >> 8;
+
+		double spd = speed[j]/0.111;
+		data[i+2] = (uint16_t)(spd < 0 ? 1 : spd) & 0xff;
+		data[i+3] = (uint16_t)(spd < 0 ? 1 : spd) >> 8;
+	}
+	
+	SyncWrite(id,numMotors,GOAL_POSITION_L, data, 4);
+	
+}
---
+++ MyDynamixel/MyDynamixel.h
@@ -0,0 +1,83 @@
+#ifndef MyDynamixel_h
+#define MyDynamixel_h
+
+#include "Arduino.h"
+
+//Possible instructions for a motor
+enum INSTRUCTION 
+{
+  PING = 0x01, READ_DATA = 0x02, WRITE_DATA = 0x03, REG_WRITE = 0x04, ACTION = 0x05, SYNC_WRITE = 0x83
+};
+
+//Control table for motors
+enum CONTROL_TABLE
+{
+  MODEL_NUMBER_L, // 0x00
+  MODEL_NUMBER_H, // 0x01
+  VERSION, // 0x02
+  ID, // 0x03
+  BAUD_RATE, // 0x04
+  RETURN_DELAY_TIME, // 0x05
+  CW_ANGLE_LIMIT_L, // 0x06
+  CW_ANGLE_LIMIT_H, // 0x07
+  CCW_ANGLE_LIMIT_L, // 0x08
+  CCW_ANGLE_LIMIT_H, // 0x09
+  RESERVED1, // 0x0A
+  HIGHEST_LIMIT_TEMPERATURE, // 0x0B
+  LOWEST_LIMIT_VOLTAGE, // 0x0C
+  HIGHEST_LIMIT_VOLTAGE, // 0x0D
+  MAX_TORQUE_L, // 0x0E
+  MAX_TORQUE_H, // 0x0F
+  STATUS_RETURN_LEVEL, // 0x10
+  ALARM_LED, // 0x11
+  ALARM_SHUTDOWN, // 0x12
+  RESERVED2, // 0x13
+  RESERVED3, // 0x14
+  RESERVED4, // 0x15
+  RESERVED5, // 0x16
+  RESERVED6, // 0x17
+  TORQUE_ENABLE, // 0x18
+  LED, // 0x19
+  CW_COMPLIANCE_MARGIN, // 0x1A
+  CCW_COMPLIANCE_MARGIN, // 0x1B
+  CW_COMPLIANCE_SLOPE, // 0x1C
+  CCW_COMPLIANCE_SLOPE, // 0x1D
+  GOAL_POSITION_L, // 0x1E
+  GOAL_POSITION_H, // 0x1F
+  MOVING_SPEED_L, // 0x20
+  MOVING_SPEED_H, // 0x21
+  TORQUE_LIMIT_L, // 0x22
+  TORQUE_LIMIT_H, // 0x23
+  PRESENT_POSITION_L, // 0x24
+  PRESENT_POSITION_H, // 0x25
+  PRESENT_SPEED_L, // 0x26
+  PRESENT_SPEED_H, // 0x27
+  PRESENT_LOAD_L, // 0x28
+  PRESENT_LOAD_H, // 0x29
+  PRESENT_VOLTAGE, // 0x2A
+  PRESENT_TEMPERATURE, // 0x2B
+  REGISTERED_INSTRUCTION, // 0x2C
+  RESERVE3, // 0x2D
+  MOVING, // 0x2E
+  LOCK, // 0x2F
+  PUNCH_L, // 0x30
+  PUNCH_H // 0x31
+};
+
+class MyDynamixel
+{
+  public:
+    	MyDynamixel(HardwareSerial& port, int rate, int Dir_pin);
+    	void WriteServos (uint8_t id,enum CONTROL_TABLE reg, uint8_t * data, uint8_t dataLength);
+    	void ReadServos (uint8_t id,enum CONTROL_TABLE reg, uint8_t dataLength, uint8_t* returnPack);
+	void MoveServos (uint8_t id, double position, double speed = -10);
+	double PresentPos (uint8_t id);
+	void SyncWrite (uint8_t* id, uint8_t numMotors, enum CONTROL_TABLE reg, uint8_t * data, uint8_t dataLength);
+	void SyncMove (uint8_t* id, double* position, double* speed, uint8_t numMotors);
+  private:
+	uint8_t goalMovementBits[4] = {0,0,0,0};
+	HardwareSerial& _port;
+	int dir_pin_;
+};
+
+#endif
