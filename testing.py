7151db449971ff2f9eafc95035134b7d2f07c2a0
EpEp <bergapie@gmail.com>
2023-04-07 13:29:28 +0200

    Walk state machine working

--- testing.py
+++ testing.py
@@ -4,11 +4,15 @@ raylib [shapes] example - Following Eyes
 
 """
 from walkStateMachine import WalkCycleMachine
-from walkStateMachine import rest_pos
+from walkStateMachine import REST_POS
 
 from pyray import *
 from raylib import colors as Color
-from numpy import deg2rad, rad2deg
+from numpy import (
+    deg2rad, rad2deg,
+    array as a
+)
+
 from math import (
     atan2,
     cos,
@@ -30,7 +34,6 @@ from raylib import (
 screenWidth = 800
 screenHeight = 450
 
-speed = 5
 body_radius = 20
 foot_radius = 10
 
@@ -40,19 +43,12 @@ body_pos = Vector2(screenWidth / 2.0 , screenHeight / 2.0)
 #             Vector2(-86.6, -50.0), Vector2(-86.6, 50.0)]
 
 # walk_direction = vector2_normalize(vector2_subtract(get_mouse_position(), body_pos))
-angle = 0.0
+# angle = 0.0
 
 walk_machine = WalkCycleMachine()
 walk_machine.active
 
-def find_active(angle):
-    if walk_machine.walk_direction is not None:
-        if (0.0 < angle and angle < deg2rad(60)) or (deg2rad(120) < angle and angle < deg2rad(180)) or (deg2rad(-120) < angle and angle < deg2rad(-60)):
-            walk_machine.active = [0,3,4]
-            walk_machine.inactive = [1,2,5]
-        else:
-            walk_machine.active = [1,2,5]
-            walk_machine.inactive = [0,3,4]
+
 
 init_window(screenWidth, screenHeight, "Walk cycle test")
 
@@ -60,24 +56,21 @@ set_target_fps(60)
 # ----------------------------------------------------------------------------------
 
 # Main game loop
+walk_direction = Vector2(0,0)
 while not window_should_close():  # Detect window close button or ESC key
+    # Input
+    # ----------------------------------------------------------------------------------
+    walk_machine.speed += get_mouse_wheel_move()*5
+    if is_mouse_button_down(MOUSE_BUTTON_LEFT):
+        walk_direction = vector2_normalize(vector2_subtract(get_mouse_position(), body_pos))
+    if is_mouse_button_down(MOUSE_BUTTON_RIGHT):
+        walk_direction = Vector2(0,0)
+    walk_machine.walk_direction = a([walk_direction.x, walk_direction.y])
+
     # Update
     # ----------------------------------------------------------------------------------
     dt = get_frame_time()
-    if is_mouse_button_down(MOUSE_BUTTON_LEFT):
-        walk_machine.walk_direction = vector2_normalize(vector2_subtract(get_mouse_position(), body_pos))
-    elif is_mouse_button_down(MOUSE_BUTTON_RIGHT):
-        walk_machine.walk_direction = Vector2(0,0)
-    angle = vector2_angle(walk_machine.walk_direction, Vector2(1,0))
-
-    find_active(angle)
-    # if walk_machine.has_speed() and walk_machine.step_finished():
-    walk_machine.walk()
-
-    if walk_machine.current_state == walk_machine.stepping:
-        for i in range(6):
-            walk_machine.foot_pos[i] = vector2_add(walk_machine.foot_pos[i], vector2_scale(vector2_subtract(walk_machine.targets[i], walk_machine.foot_pos[i]), speed*dt))
-   
+    walk_machine.update(dt)
     # ----------------------------------------------------------------------------------
 
     # draw
@@ -85,33 +78,36 @@ while not window_should_close():  # Detect window close button or ESC key
     begin_drawing()
 
     clear_background(Color.RAYWHITE)
-
+    
+    # Draw Legs
+    for i in range(6):
+        if walk_machine.active[i]:
+            color = Color.GREEN
+        else:
+            color = Color.RED
+        foot_pos_screen = walk_machine.foot_pos[i] + a([body_pos.x, body_pos.y])
+        foot_pos_screen = Vector2(foot_pos_screen[0], foot_pos_screen[1])
+        foot_target_screen = walk_machine.targets[i] + a([body_pos.x, body_pos.y])
+        draw_line_ex(body_pos, foot_pos_screen,2, color)
+        draw_circle_v(Vector2(foot_target_screen[0], foot_target_screen[1]), foot_radius, Color.GRAY)
+        draw_circle_v(foot_pos_screen, foot_radius, color)
+    
     # Draw body
     draw_circle_v(body_pos, body_radius, Color.RED)
-    
-    # Draw feet
-    for i in walk_machine.active:
-        draw_circle_v(vector2_add(walk_machine.targets[i], body_pos), foot_radius, Color.GRAY)
-        draw_circle_v(vector2_add(walk_machine.foot_pos[i], body_pos), foot_radius, Color.GREEN)
 
-    for i in walk_machine.inactive:
-        draw_circle_v(vector2_add(walk_machine.targets[i], body_pos), foot_radius, Color.GRAY)
-        draw_circle_v(vector2_add(walk_machine.foot_pos[i], body_pos), foot_radius, Color.RED)
-    
-    
     # Draw direction
-    if not vector2_equals(walk_machine.walk_direction, Vector2(0,0)) :
-        draw_line_ex(body_pos, vector2_add(body_pos, vector2_scale(walk_machine.walk_direction, 100)),5,Color.RED)
+    if not vector2_equals(walk_direction, Vector2(0,0)) :
+        draw_line_ex(body_pos, vector2_add(body_pos, vector2_scale(walk_direction, 100)),5,Color.RED)
     
     # Draw text
-    draw_fps(10, 10)
     if walk_machine.current_state == walk_machine.rest:
         color = Color.RED
     elif walk_machine.current_state == walk_machine.stepping:
         color = Color.GREEN
-    draw_text(walk_machine.current_state.name, 10, 30, 20, color)
-    draw_text(str(rad2deg(angle)), 10, 50, 20, Color.GRAY)
-    draw_text(str(walk_machine.step_finished()), 10, 70, 20, Color.GRAY)
+    draw_text(walk_machine.current_state.name, 10, 10, 20, color)
+    draw_text("Speed: %.2f" % walk_machine.speed, 10, 30, 20, Color.BLACK)
+    draw_text("Angle: %.2f" % rad2deg(walk_machine.angle), 10, 50, 20, Color.BLACK)
+
 
     end_drawing()
 
--- walkStateMachine.py
+++ walkStateMachine.py
@@ -1,59 +1,135 @@
 from statemachine import StateMachine, State
+import numpy as np
+from numpy import array as a
+from numpy import deg2rad, rad2deg
+from math import acos, sqrt
 from pyray import *
 
-rest_pos = [Vector2(86.6, -50.0), Vector2(86.6, 50.0),
-                Vector2(0, -100.0), Vector2(0, 100.0),
-                Vector2(-86.6, -50.0), Vector2(-86.6, 50.0)]
+# rest_pos = [Vector2(86.6, -50.0), Vector2(86.6, 50.0),
+#                 Vector2(0, -100.0), Vector2(0, 100.0),
+#                 Vector2(-86.6, -50.0), Vector2(-86.6, 50.0)]
 
-stride_length = 30
+REST_POS = [a([86.6, -50.0]), a([86.6, 50.0]),
+            a([0.0, -100.0]), a([0.0, 100]),
+            a([-86.6, -50.0]), a([-86.6, 50.0])]
 
-def vector2_close(a,b, close):
-    diff_x = abs(a.x - b.x)
-    diff_y = abs(a.y - b.y)
-    if diff_x < close and diff_y < close:
-        return True
-    return False
+STRIDE_LENGTH = 30
+PLACE_TOLERANCE = 1
+
+def find_angle(v):
+    # return np.angle(v)
+    if v[1] > 0:        
+        return -acos(np.clip((v@a([1,0]))/sqrt(v@v), -1.0, 1.0))
+    else:
+        return acos(np.clip((v@a([1,0]))/sqrt(v@v), -1.0, 1.0))
+
+def normalize(v):
+    return v/sqrt(v@v)
 
 class WalkCycleMachine(StateMachine):
     "A walk cycle machine"
-    rest = State(initial=True)
-    stepping = State()
+    rest = State(initial=True, enter="deactivate_all")
+    stepping = State(enter="find_active")
      
     walk = rest.to(stepping, cond="has_speed") | stepping.to(rest, cond="step_finished") | rest.to.itself(internal=True) | stepping.to.itself(internal=True) 
 
     def __init__(self):
-        self.active = [None, None, None]
-        self.inactive = [None, None, None]
-        self.walk_direction = Vector2(0,0)
-        self.foot_pos = [Vector2(86.6, -50.0), Vector2(86.6, 50.0),
-                Vector2(0, -100.0), Vector2(0, 100.0),
-                Vector2(-86.6, -50.0), Vector2(-86.6, 50.0)]
-        self.targets = [Vector2(86.6, -50.0), Vector2(86.6, 50.0),
-                Vector2(0, -100.0), Vector2(0, 100.0),
-                Vector2(-86.6, -50.0), Vector2(-86.6, 50.0)]
+        self.active = a([False, False, False, False, False, False])
+        self.stretch = [None,None]
+        self.angle = 0.0
+        self.speed = 50
+        self.walk_direction = a([0,0])
+        self.foot_pos = [a([86.6, -50.0]), a([86.6, 50.0]),
+            a([0.0, -100.0]), a([0.0, 100]),
+            a([-86.6, -50.0]), a([-86.6, 50.0])]
+        self.targets = [a([86.6, -50.0]), a([86.6, 50.0]),
+            a([0.0, -100.0]), a([0.0, 100]),
+            a([-86.6, -50.0]), a([-86.6, 50.0])]
         
         super(WalkCycleMachine, self).__init__()
 
-    def on_enter_stepping(self):
-        self.update_targets()
+    # Enter actions
+    # -------------------------------------------------------------------------------------------
+    def deactivate_all(self):
+        self.active[0] = False
+        self.active[1] = False
+        self.active[2] = False
+        self.active[3] = False
+        self.active[4] = False
+        self.active[5] = False
 
+    def find_active(self):
+        angle = find_angle(self.walk_direction)
+        self.angle = angle
+        if self.walk_direction is not None:
+            id = -1
+            if 0.0 < angle and angle < deg2rad(60):
+                id = 0
+            elif deg2rad(60) < angle and angle < deg2rad(120):
+                id = 2
+            elif deg2rad(120) < angle and angle < deg2rad(180):
+                id = 4
+            elif deg2rad(-180) < angle and angle < deg2rad(-120):
+                id = 5
+            elif deg2rad(-120) < angle and angle < deg2rad(-60):
+                id = 3
+            elif deg2rad(-60.0) < angle and angle < 0.0:
+                id = 1
+            if id == 0 or id == 3 or id == 4:
+                self.active[0] = True                
+                self.active[1] = False
+                self.active[2] = False
+                self.active[3] = True
+                self.active[4] = True
+                self.active[5] = False
+            elif id == 1 or id == 2 or id == 5:
+                self.active[0] = False
+                self.active[1] = True
+                self.active[2] = True
+                self.active[3] = False
+                self.active[4] = False
+                self.active[5] = True
+            else:
+                print("id not found")
+            if self.is_long(id):
+                self.active = np.invert(self.active)
+    # -------------------------------------------------------------------------------------------
+    
+    # Conditions
+    # -------------------------------------------------------------------------------------------
     def step_finished(self):
         for i in range(6):
-            if not vector2_close(self.foot_pos[i], self.targets[i], 5):
+            if not (abs(self.foot_pos[i] - self.targets[i]) < PLACE_TOLERANCE).all():
                 return False
         return True
     
     def has_speed(self):
-        return not vector2_equals(self.walk_direction, Vector2(0,0))
+        return not (self.walk_direction == a([0,0])).all()
+    # -------------------------------------------------------------------------------------------
+
+    # Logic
+    # -------------------------------------------------------------------------------------------
+    def update(self, dt):
+        self.update_targets()
+        self.walk()
+
+        if self.current_state == self.stepping:
+            for i in range(6):
+                self.foot_pos[i] = self.foot_pos[i] + (normalize(self.targets[i] - self.foot_pos[i])*self.speed*dt)
 
     def update_targets(self):
-        for i in self.active:
-            if i is not None:
-                self.targets[i] = vector2_add(rest_pos[i], vector2_scale(self.walk_direction,stride_length))
-        for i in self.inactive:
-            if i is not None:
-                self.targets[i] = vector2_subtract(rest_pos[i], vector2_scale(self.walk_direction,stride_length))
+        for i in range(6):
+            if self.active[i]:
+                self.targets[i] = REST_POS[i] + (self.walk_direction * STRIDE_LENGTH)
+            else:
+                self.targets[i] = REST_POS[i] - (self.walk_direction * STRIDE_LENGTH)
+    
+    # -------------------------------------------------------------------------------------------
 
+    def is_long(self, id):
+        if self.foot_pos[id]@self.foot_pos[id] > REST_POS[id]@REST_POS[id]:
+            return True
+        return False
 
 
 if __name__ == '__main__':
