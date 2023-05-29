Surface map worlking with rotation, need to fix removal

perception.py
@@ -1,6 +1,6 @@
 import numpy as np
 from multiprocessing import shared_memory
-from roboMath import rotate_vec
+from roboMath import rotate_vec, rotate_vec_quat
 EXTENTS = 30                        # Extents of SDF block, in distance units
 DIVISIOINS = 4                      # Cells per distance unit
 SDF_EXTENTS = EXTENTS*DIVISIOINS    # Extents of SDF block, in number of cells
@@ -9,7 +9,7 @@ def to_sdf_index(global_pos):
     """Convert global position to position in SDF grid, which has its corner at 0,0,0"""
     return ((global_pos)%(EXTENTS))*DIVISIOINS
 
-f = lambda x: rotate_vec(x, np.array([1,0,0]), -0.54)
+
 
 class Perception():
     
@@ -31,11 +31,13 @@ class Perception():
         self.cell_offset = np.zeros(3) # Position offset from cell origin
 
     
-    def update(self, global_pos, points):
+    def update(self, global_pos, body_quaternion, points):
+        # f = lambda x: rotate_vec(x, np.array([1,0,0]), -0.54)
+        f2 = lambda x: rotate_vec_quat(x, body_quaternion)
         self.update_sdf_index(global_pos)
-        indices = ((f(points) + EXTENTS/2)*DIVISIOINS).astype(int)
+        indices = ((f2(points) + EXTENTS/2)*DIVISIOINS).astype(int)
         # self.sdf_buffer[:] = 100
-        self.sdf_buffer[(indices[:,0] + self.sdf_index[1])%SDF_EXTENTS, (indices[:,1] - self.sdf_index[2])%SDF_EXTENTS, (indices[:,2] + self.sdf_index[0])%SDF_EXTENTS] = 0.0
+        self.sdf_buffer[(indices[:,0] - self.sdf_index[0])%SDF_EXTENTS, (indices[:,1] - self.sdf_index[1])%SDF_EXTENTS, (indices[:,2] - self.sdf_index[2])%SDF_EXTENTS] = 0.0
         
 
     def update_sdf_index(self, global_pos):
@@ -45,24 +47,25 @@ class Perception():
         local_new = local_new.astype(int)
         diff = local_new - self.sdf_index
         self.sdf_index[:] = local_new[:]
-        print(f"Global: {np.array2string(global_pos,precision=2, floatmode='fixed')}        Index: {self.sdf_index}         Offset: {np.array2string(self.cell_offset,precision=2,floatmode='fixed')}")
+        # print(f"Global: {np.array2string(global_pos,precision=2, floatmode='fixed')}        Index: {self.sdf_index}         Offset: {np.array2string(self.cell_offset,precision=2,floatmode='fixed')}")
         # print(diff)
 
         # Clear required cells
         #-------------------------------------------------------------
         # Y axis
+        print(diff)
         if diff[0] > 0:
             start = int(self.sdf_index[0]+SDF_EXTENTS-diff[0])%(SDF_EXTENTS)
             end = int(self.sdf_index[0]+SDF_EXTENTS)%(SDF_EXTENTS)
             if end == 0:
                 end = SDF_EXTENTS
-            self.sdf_buffer[:,:,start:end] = 100
+            self.sdf_buffer[start:end,:,:] = 100
         elif diff[0] < 0:
             start = int(self.sdf_index[0])%(SDF_EXTENTS)
             end = int(self.sdf_index[0]-diff[0])%(SDF_EXTENTS)
             if end == 0:
                 end = SDF_EXTENTS
-            self.sdf_buffer[:,:,start:end] = 100
+            self.sdf_buffer[start:end,:,:] = 100
         
         # X axis
         if diff[1] > 0:
@@ -70,13 +73,13 @@ class Perception():
             end = int(self.sdf_index[1]+SDF_EXTENTS)%(SDF_EXTENTS)
             if end == 0:
                 end = SDF_EXTENTS
-            self.sdf_buffer[start:end,:,:] = 100
+            self.sdf_buffer[:,start:end,:] = 100
         elif diff[1] < 0:
             start = int(self.sdf_index[1])%(SDF_EXTENTS)
             end = int(self.sdf_index[1]-diff[1])%(SDF_EXTENTS)
             if end == 0:
                 end = SDF_EXTENTS
-            self.sdf_buffer[start:end,:,:] = 100
+            self.sdf_buffer[:,start:end,:] = 100
 
         # Z axis
         if diff[2] > 0:
@@ -84,11 +87,11 @@ class Perception():
             end = int(self.sdf_index[2]+SDF_EXTENTS)%(SDF_EXTENTS)
             if end == 0:
                 end = SDF_EXTENTS
-            self.sdf_buffer[:,start:end,:] = 100
+            self.sdf_buffer[:,:,start:end] = 100
         elif diff[2] < 0:
             start = int(self.sdf_index[2])%(SDF_EXTENTS)
             end = int(self.sdf_index[2]-diff[2])%(SDF_EXTENTS)
             if end == 0:
                 end = SDF_EXTENTS
-            self.sdf_buffer[:,start:end,:] = 100
+            self.sdf_buffer[:,:,start:end] = 100
         #-------------------------------------------------------------