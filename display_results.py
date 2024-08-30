6e3dc7fa76a9012860c862dce27f4faf580e6e8d
EpEp <bergapie@gmail.com>
2024-08-30 15:08:29 +0200

    Results viz

--- display_results.py
+++ display_results.py
@@ -6,6 +6,16 @@ import matplotlib.pyplot as plt
 import csv
 from catkin_ws.src.hexapod_ros.scripts.roboMath import rotate
 
+cm = 1/2.54
+textwidth = 13.98611*cm
+
+normalsize = 12
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "Cambria",
+    "font.size": normalsize
+})
+
 
 with open(os.path.join("Results", "PoseData.csv"),'r') as file:
     row_count = sum(1 for lines in csv.reader(file))
@@ -16,17 +26,20 @@ pose_line = next(csvfile)
 pose_data = np.zeros((row_count,8))
 fig, ax = plt.subplots(2,1)
 plt.figure(1)
-ax[0].xaxis.set_inverted(True)
-ax[1].xaxis.set_inverted(True)
-ax[0].axis('equal')
-ax[1].axis('equal')
-plt.xlabel('Side to side position (Y)')
-ax[0].set(ylabel='Forward Backwards (X)')
-ax[1].set(ylabel='Up Down (Z)')
-plt.gca().set_aspect('equal')
+# ax[0].xaxis.set_inverted(True)
+# ax[1].xaxis.set_inverted(True)
+# ax[0].axis('equal')
+# ax[1].axis('equal')
+plt.xlabel('Side to side position (X, cm)')
+ax[0].set(ylabel='Forward Backwards (Y, cm)')
+ax[1].set(ylabel='Up Down (Z, cm)')
+# plt.gca().set_aspect('equal')
 
 plt.figure(2)
 plt.title("Heightmap")
+plt.xlabel("X (cm)")
+plt.ylabel("Y (cm)")
+cbar = None
 
 # ax[0].set_xlim(0.01, -0.2)
 # ax[0].set_ylim(-0.13, 0.02)
@@ -73,28 +86,39 @@ for hmap_file in hmap_files:
     hmap_frames.append(np.load(os.path.join(path_hmap,hmap_file)))
 
 print((int(pose_line[0])-int(os.path.splitext(color_files[0])[0]))/1_000_000_000)
-
+first = True
+first_color = True
 for i in range(len(color_files)-1):
     t_now = time.time()
     if frametimes[i] >= 25.0:
         cv2.imshow("Color", cv2.imread(os.path.join(path_color, color_files[i]))[:,:,[2,1,0]])
-        if i_depth < len(depth_frames) and frametimes[i] >= (int(os.path.splitext(depth_files[i_depth])[0]) - int(os.path.splitext(color_files[0])[0]))/1_000_000_000:
-            plt.figure(3)
-            plt.imshow(depth_frames[i_depth])
+        # if i_depth < len(depth_frames) and frametimes[i] >= (int(os.path.splitext(depth_files[i_depth])[0]) - int(os.path.splitext(color_files[0])[0]))/1_000_000_000:
+            # plt.figure(3)
+            # plt.imshow(depth_frames[i_depth])
             # cv2.imshow("Hmap",depth_frames[i_depth])
-            i_depth += 1
+            # i_depth += 1
         if frametimes[i] >= (int(pose_line[0])-int(os.path.splitext(color_files[0])[0]))/1_000_000_000 and not pose_end:
             hmap_i = hmap_files.index(pose_line[0]+".npy")
             if hmap_i is not None:
                 hmap = hmap_frames[hmap_i]
                 plt.figure(2)
                 hmap[:5,:5] = 0
-                plt.imshow(np.flip(hmap.transpose(),1),extent=(hmap.shape[0]*0.5, -hmap.shape[0]*0.5, -hmap.shape[1]*0.5, hmap.shape[1]*0.5), cmap='plasma')
+                himg = plt.imshow(np.flip(hmap.transpose(),1)*10,extent=(-hmap.shape[0]*0.25*1.3333, hmap.shape[0]*0.25*1.3333, -hmap.shape[1]*0.25*1.3333, hmap.shape[1]*0.25*1.3333), cmap='plasma')
+                if cbar:
+                    cbar.remove()
+                    cbar = plt.colorbar(himg)
+                    cbar.set_label("cm")
+                else:
+                    cbar = plt.colorbar(himg)
+                    cbar.set_label("cm")
                 # cv2.imshow("Hmap", np.flip(hmap.transpose(),1))
-                plt.show()
                 
                 # cv2.imshow("Hmap", hmap)
+
             pose_data[pose_i] = pose_line
+            pose_data[pose_i][0] *= 10
+            pose_data[pose_i][1] *= 10
+            pose_data[pose_i][2] *= -10
             # pose_data[pose_i][1:4] = rotate(np.array([np.sin(angle)*1, np.sin(angle)*0, np.sin(angle)*0, np.cos(angle)]), np.array(pose_line[1:4],dtype=np.float))
             try:
                 pose_line = next(csvfile)
@@ -123,11 +147,27 @@ for i in range(len(color_files)-1):
                 ax[1].plot(pose_data[pose_i-1, 2], pose_data[pose_i-1, 3], 'o', color='red')
                 ax[1].plot(pose_data[0, 2], pose_data[0, 3], 'o', color='blue')
                 plt.draw()
+                plt.pause(0.0001)
+                
+                if first and frametimes[i] >= 25:
+                    first = False
+                    plt.figure(2)
+                    fig.set_figwidth(textwidth)
+                    plt.savefig("hmap_start.pdf", format="pdf", bbox_inches="tight")
+                    cv2.imwrite("color_start.jpeg", cv2.imread(os.path.join(path_color, color_files[i]))[:,:,[2,1,0]])
+                elif first_color and i > 1300:
+                    first_color = False
+                    plt.figure(2)
+                    fig.set_figwidth(textwidth)
+                    plt.savefig("hmap_half.pdf", format="pdf", bbox_inches="tight")
+                    cv2.imwrite("color_half.jpeg", cv2.imread(os.path.join(path_color, color_files[i]))[:,:,[2,1,0]])
+
         cv2.waitKey(1)
         t_delta_true = time.time() - t_now
-        print(t_delta_true, delta_frametimes[i])
-        time.sleep(delta_frametimes[i] - (t_now - time.time()))
+        print(t_delta_true, delta_frametimes[i], i)
+        # time.sleep(delta_frametimes[i] - (t_now - time.time()))
 cv2.imshow("Color", cv2.imread(os.path.join(path_color, color_files[-1]))[:,:,[2,1,0]])
+cv2.imwrite("color.jpeg", cv2.imread(os.path.join(path_color, color_files[i]))[:,:,[2,1,0]])
 
 # while True:
 #     angle += np.deg2rad(1)
@@ -141,7 +181,10 @@ cv2.imshow("Color", cv2.imread(os.path.join(path_color, color_files[-1]))[:,:,[2
 #     ax[1].plot(-pose_data[-1, 1], -pose_data[-1, 2], 'o', color='red')
 #     ax[1].plot(-pose_data[0, 1], -pose_data[0, 2], 'o', color='blue')
 #     plt.draw()
-
+plt.figure(1)
+fig.set_figwidth(textwidth)
+plt.savefig("pos.pdf", format="pdf", bbox_inches="tight")
 plt.figure(2)
-plt.savefig("myImagePDF.pdf", format="pdf", bbox_inches="tight")
+fig.set_figwidth(textwidth)
+plt.savefig("hmap.pdf", format="pdf", bbox_inches="tight")
 input("Enter end")
